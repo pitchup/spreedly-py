9121afd58973fe01c9feb3dc1dfb3601b8bc2b91
Mike Codona <mike@pitchup.com>
2014-01-30 14:22:18 +0000

    Remove scripts from setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author='M. Codona',
     author_email='mike@pitchup.com',
     packages=['spreedly'],
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
     description='Python port of spreedly-gem.',
     long_description=open('README.md').read(),
     install_requires=[
